ebef38d1c8653e4f3513109bbd175d849238fd58
unknown <sarvgt435216@gmail.com>
2022-10-20 08:23:55 +0530

    Modified App.js by removing useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Components/Signup/Login';
 import Forgotpwd from './Components/Signup/forgotpwd';
